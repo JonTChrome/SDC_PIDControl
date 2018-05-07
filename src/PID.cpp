8c0979b1ffa0b10f5910a2e5884c4dc83afa7acd
jtracy9 <jtracy9@ford.com>
2018-05-07 11:09:03 -0400

    submission

--- src/PID.cpp
+++ src/PID.cpp
@@ -11,11 +11,20 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+    PID::Kp = Kp;
+    PID::Ki = Ki;
+    PID::Kd = Kd;
 }
 
 void PID::UpdateError(double cte) {
+    d_error = cte - p_error;
+
+    p_error = cte;
+    
+    i_error += cte;
 }
 
 double PID::TotalError() {
+    return -p_error * Kp - i_error * Ki - d_error * Kd;
 }
 
